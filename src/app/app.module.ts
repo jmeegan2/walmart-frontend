d82e982a5eeb55f51af3cbdab68f2f09ef57e2b5
James Meegan <jmeegan2@owlmail.harford.edu>
2022-04-04 21:47:49 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mock-users.ts
@@ -0,0 +1,16 @@
+// import { Hero } from './';
+
+// export const HEROES: Hero[] = [
+//   { id: 1, name: 'James Meegan' },
+//   { id: 2, name: 'John Lynch' },
+//   { id: 3, name: 'Mary Clark' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' }
+// ];
+
+
